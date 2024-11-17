94a537a9f8e24558035761918119ea3f94d70337
JahongirBozorov <128155644+JahongirBozorov@users.noreply.github.com>
2024-11-17 11:14:04 +0500

    rahbariyat

--- src/pages/Rahbariyat/Rahbariyat.jsx
+++ src/pages/Rahbariyat/Rahbariyat.jsx
@@ -1,72 +1,35 @@
 import React from 'react';
 import './Rahbariyat.css';
-import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
-import Blog1Img from '../../utils/images/blog1-img.jpg';
-import Blog2Img from '../../utils/images/blog2-img.jpg';
-import Blog3Img from '../../utils/images/blog3-img.jpg';
-import Blog4Img from '../../utils/images/blog4-img.jpg';
-import Blog5Img from '../../utils/images/blog5-img.jpg';
-import Blog6Img from '../../utils/images/blog6-img.jpg';
-import Blog7Img from '../../utils/images/blog7-img.jpg';
-import Blog8Img from '../../utils/images/blog8-img.jpg';
-import Blog9Img from '../../utils/images/blog9-img.jpg';
-
+import Blog1Img from '../../utils/Rahbariyat/jumaqulov Golib.png';
+import Blog2Img from '../../utils/Rahbariyat/mamirov Feruz.png';
+import Blog3Img from '../../utils/Rahbariyat/Nazarov Nozim.png';
 const blogs = [
     {
         id: 1,
         img: [Blog1Img],
-        title: 'Blog 1',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        name: "G'olibjon",
+        firstname:"Jumaqulov",
+        lastname:"Hasanovich",
+        description: "Buxoro davlat universiteti Qorako'l akademik litseyi direktori"
     },
     {
         id: 2,
         img: [Blog2Img],
-        title: 'Blog 2',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        name: "Feruz",
+        firstname:"Mamirov",
+        lastname:"To'xtaxonovich",
+        description: " Buxoro davlat universiteti  Qorako‘l akademik litseyi Yoshlar masalalari va ma’naviy va ma’rifiy ishlar bo’yicha direktor o’rinbosari"
     },
     {
         id: 3,
         img: [Blog3Img],
-        title: 'Blog 3',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        name: "Nozim",
+        firstname:"Nazarov",
+        lastname:"Umidjon o'g'li",
+        description: "Бухоро давлат университети Қоракўл академик лицейи Бўлим мудири (Бошлиғи)"
     },
-    {
-        id: 4,
-        img: [Blog4Img],
-        title: 'Blog 4',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
-    {
-        id: 5,
-        img: [Blog5Img],
-        title: 'Blog 5',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
-    {
-        id: 6,
-        img: [Blog6Img],
-        title: 'Blog 6',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
-    {
-        id: 7,
-        img: [Blog7Img],
-        title: 'Blog 7',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
-    {
-        id: 8,
-        img: [Blog8Img],
-        title: 'Blog 8',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
-    {
-        id: 9,
-        img: [Blog9Img],
-        title: 'Blog 9',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
+    
 ];
 
 function Rahbariyat() {
@@ -83,20 +46,19 @@ function Rahbariyat() {
                 <div className="row g-4">
                     {blogs.map((blog) => (
                         <div key={blog.id} className='col-md-6 col-lg-4'>
-                            <Link to="/blog" className='text-decoration-none'>
                                 <Card className='h-100 shadow scale-hover-effect bg-dark text-light border-0'>
-                                    <Card.Img variant="top" src={blog.img} />
+                                    <Card.Img variant="top"src={blog.img} />
                                     <Card.Body className='p-md-5 d-flex flex-column align-items-center'>
-                                        <Card.Title className='fs-2 mb-4'>{blog.title}</Card.Title>
+                                        <Card.Title className='fs-2 mb-4 rahbarname'>{blog.firstname} {blog.name} {blog.lastname}</Card.Title>
                                         <Card.Text className='text-center'>{blog.description}</Card.Text>
                                     </Card.Body>
                                 </Card>
-                            </Link>
                         </div>
                     ))}
                 </div>
             </div>
         </div>
+        <hr />
     </div>
   )
 }
